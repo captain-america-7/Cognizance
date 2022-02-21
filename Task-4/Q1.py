2dc9c9c9fd7fb0d724df9d3032a8e10577025c71
captain-america-7 <99639636+captain-america-7@users.noreply.github.com>
2022-02-21 21:06:40 +0530

    Add files via upload

---
+++ Task-4/Q1.py
@@ -0,0 +1,6 @@
+#Created and Developed by Aruntej
+#Program to perform addition on two user input numbers
+num_1=int(input("Enter the first number:"))
+num_2=int(input("Enter the second number:"))
+Sum=num_1+num_2
+print("Sum of the two numbers is:-",Sum)
